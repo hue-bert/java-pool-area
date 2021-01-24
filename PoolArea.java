198e755765e6235c3c9f892b816e74dccfa7aa5a
hue-bert <hubert.okoe@amalitech.org>
2021-01-24 18:36:01 +0100

    Initial commit

---
+++ PoolArea.java
@@ -0,0 +1,60 @@
+public class PoolArea {
+
+    public class Rectangle {
+        private double width;
+        private double length;
+
+        public Rectangle(double width, double length) {
+            this.width = width;
+            this.length = length;
+
+            if (width < 0) {
+                this.width = 0;
+            } else {
+                this.width = width;
+            }
+            if (length < 0) {
+                this.length = 0;
+            } else {
+                this.length = length;
+            }
+        }
+
+        public double getWidth() {
+            return width;
+        }
+
+        public double getLength() {
+            return length;
+        }
+
+        public double getArea() {
+            return width * length;
+        }
+
+    }
+
+    public class Cuboid extends Rectangle {
+        private double height;
+
+
+        public Cuboid(double width, double length, double height) {
+            super(width, length);
+
+            if (height < 0) {
+                this.height = 0;
+            }
+            else {
+                this.height = height;
+            }
+        }
+
+        public double getHeight() {
+            return this.height;
+        }
+
+        public double getVolume() {
+            return getArea() * height;
+        }
+    }
+}
